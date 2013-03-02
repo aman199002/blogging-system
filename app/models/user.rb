efa79f4b90c9f7ed13bc39eaf8a3695b3092f420
aman <aman@ubuntu.ubuntu-domain>
2013-03-02 21:59:27 +0530

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   validates :email, :presence => true, :uniqueness => {:case_sensitive => false}
   validates :password, :confirmation => true
 
-  before_save :encrypt_password
+  before_create :encrypt_password
 
   def self.authenticate(email, password)
     user = find_by_email(email)    
