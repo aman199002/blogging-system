8fe8da081303bdfd1f50c4d2018c0df3431d4706
aman <aman@ubuntu.ubuntu-domain>
2013-03-02 20:56:29 +0530

    update seed to make admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create(:email => "admin@gmail.com", :password => "password", :confirm_password => "password").make_admin
